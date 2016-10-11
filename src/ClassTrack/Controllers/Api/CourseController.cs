d67ecf4e0c99e14c37f65ca9e13966b50e380460
IanStodart <icsaxophone1745@hotmail.com>
2016-10-11 13:20:04 -0700

    Fixing my test method to Test4, was accidently a 3

--- src/ClassTrack/Controllers/Api/CourseController.cs
+++ src/ClassTrack/Controllers/Api/CourseController.cs
@@ -40,7 +40,7 @@ namespace ClassTrack.Controllers.Api
 
         //Ian Stodart Assignment 3 Part 3
         [HttpGet("api/test3")]
-        public IActionResult Test3()
+        public IActionResult Test4()
         {
             return Ok("3 Test methods are not enough, we need a 4th! - Ian xD");
         }
