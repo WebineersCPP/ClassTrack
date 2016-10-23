830700a04ee6f4d6e250f56cf920ac5408816966
Paul Chiou <hellotracybaby@yahoo.com>
2016-10-23 14:48:05 -0700

    Implemented HTMLToCurriculumSheetService to retrive information from a HTML catalog link. Convert useful data in the desired Curriculum Sheet structure and return such CurriculumSheet object.

---
+++ src/ClassTrack/Controllers/Api/TestHTMLToCSController.cs
@@ -0,0 +1,57 @@
+﻿using ClassTrack.Models;
+using ClassTrack.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTrack.Controllers.Api
+{
+    public class TestHTMLToCSController : Controller
+    {
+        // Here we are maintaining our private variable, which will be initialized in contructor
+        private GeoCoordsService _coordsService;
+
+        public TestHTMLToCSController(GeoCoordsService coordsService)
+        {
+            // we are injecting this service in this controller so it can use it
+            // recall that this service is injectable because we specified it in Startup.cs under ConfigureServices() line 51 services.AddTransient<GeoCoordsService>();
+            _coordsService = coordsService;
+        }
+
+        [HttpGet("api/testHTMLToCS")]
+        // for some reason this returns a 401 error. :(
+        // but either way, say method from service works, it would return our expected result
+        public async Task<IActionResult> Get()
+        {
+
+
+            // TESTING THE HTMLToCurriculumSheetService BY CALLING IT **SEE DEBUGGING CONSOLE FOR RETURNED CURRICULUM SHEET**
+            // PASS IN ANY CATALOG URL
+            // BREAKPOINT IS SET IN THE LINE OF RETURN AT THE END OF HTMLToCurriculumSheetService CLASS
+            HTMLToCurriculumSheetService testService = new HTMLToCurriculumSheetService();
+            string url = "https://catalog.cpp.edu/preview_program.php?catoid=4&poid=983&returnto=751";
+            CurriculumSheet returnTestCS = await testService.getCurriculumSheet(url);
+
+
+
+
+            // Use the method GetCoordAsync from the service GeoCoordsService.cs
+            var result = await _coordsService.GetCoordsAsync("Atlanta, GA");
+            if (!result.Success)
+            {
+                throw new Exception(result.Message);
+            }
+            else
+            {
+                GeoCoordsResult var = new GeoCoordsResult();
+                var.Latitude = result.Latitude;
+                var.Longitude = result.Longitude;
+
+                return Ok(var);
+            }
+        }
+    }
+}
---
+++ src/ClassTrack/Services/HTMLToCurriculumSheetService.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net.Http;
+using ClassTrack.Models;
+using System.Net;
+
+namespace ClassTrack.Services
+{
+    public class HTMLToCurriculumSheetService
+    {
+        List<CourseItem> courseItemList = new List<CourseItem>();
+
+        static Stack<Module> tempModules = new Stack<Module>();
+        static Stack<Module> modules = new Stack<Module>();
+
+        static List<Module> modulesList = new List<Module>();
+        static CurriculumSheet cs = new CurriculumSheet();
+
+        static List<Item> courseList;
+        static bool listOpen = false;
+
+        public async Task<CurriculumSheet> getCurriculumSheet(string url)
+        {
+            // Set up HtmlAgilityPack to fetch html data
+            HtmlAgilityPack.HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlDocument html = await new HtmlWeb().LoadFromWebAsync(url);
+            var root = html.DocumentNode;
+            var htmlNodes = root.Descendants();
+
+            // Search through fetched html nodes for relevant information
+            foreach (HtmlNode node in htmlNodes)
+            {
+                Module currentModule;
+
+                // Module with h2 tag
+                if (node.Name == "h2")
+                {
+                    if (listOpen == true)
+                    {
+                        tempModules.Peek().Items = courseList;
+                        listOpen = false;
+                    }
+
+                    currentModule = new Module();
+                    currentModule.Title = node.InnerText;
+                    currentModule.IsSubmodule = false;
+
+                    tempModules.Push(currentModule);
+                }
+
+                // Submodule with h3 tag
+                if (node.Name == "h3")
+                {
+                    if (listOpen == true)
+                    {
+                        tempModules.Peek().Items = courseList;
+                        listOpen = false;
+                    }
+
+                    currentModule = new Module();
+                    currentModule.Title = node.InnerText;
+                    currentModule.IsSubmodule = true;
+
+                    tempModules.Push(currentModule);
+                }
+
+                // Courses with acalog-course class attribute
+                if (node.GetAttributeValue("class", "").Equals("acalog-course"))
+                {
+                    // New course list in new module/submodule
+                    if (listOpen == false)
+                    {
+                        courseList = new List<Item>();
+                        listOpen = true;
+                    }
+
+                    CourseItem course = new CourseItem();
+
+                    // Markers to retrieve course title & course number in Course text 
+                    int titleStartIndex = 0;
+                    int titleEndIndex = 0;
+                    int numberEndIndex = 0;
+
+                    string courseText = node.InnerText;
+
+                    // Scan Course text for relavent patterns
+                    for (int i = 0; i < courseText.Length - 3; i++)
+                    {
+                        if (courseText.Substring(i, 3).Equals(" - "))
+                        {
+                            numberEndIndex = i;
+                            titleStartIndex = i + 3;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < courseText.Length - 1; i++)
+                    {
+                        if (courseText.Substring(i, 1).Equals("("))
+                        {
+                            titleEndIndex = i - 1;
+                            break;
+                        }
+                    }
+
+                    // Account for possible course units scenarios
+                    if (courseText.Contains("(1)"))
+                        course.Units = 1;
+                    else if (courseText.Contains("(2)"))
+                        course.Units = 2;
+                    else if (courseText.Contains("(3)"))
+                        course.Units = 3;
+                    else if (courseText.Contains("(4)"))
+                        course.Units = 4;
+                    else if (courseText.Contains("1-2"))
+                        course.Units = 2;
+                    else if (courseText.Contains("1-3"))
+                        course.Units = 3;
+                    else if (courseText.Contains("1-4"))
+                        course.Units = 4;
+                    else
+                        course.Units = 4;
+
+                    // Assign retrieved course into
+                    course.Number = courseText.Substring(0, numberEndIndex);
+                    course.Title = courseText.Substring(titleStartIndex, titleEndIndex - titleStartIndex);
+
+                    courseList.Add(course);
+                }
+            }
+
+            // Reverse temporary stack data structure
+            while (tempModules.Count != 0)
+            {
+                modules.Push(tempModules.Pop());
+            }
+
+            modulesList = modules.ToList();
+            cs.Modules = modulesList;
+
+            // Return curriculum sheet
+            return cs;
+        }
+    }
+}
+
+
