c3a45bf8265b09986e6de87af63e9b4ae51caa59
nliupeng <nliupeng@gmail.com>
2016-11-01 00:59:44 -0700

    updated UI interface and added additional views

---
+++ src/ClassTrack/wwwroot/js/app-home.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    // create the module
+    angular.module("app-home", ["ngRoute"])
+        .config(function ($routeProvider) {
+            $routeProvider.when("/", {
+                controller: "homeController",
+                controllerAs: "vm",
+                templateUrl: "/views/homeView.html"
+            });
+
+            $routeProvider.otherwise({ redirectTo: "/" });
+        });
+
+})();
---
+++ src/ClassTrack/wwwroot/js/homeController.js
@@ -0,0 +1,27 @@
+﻿// homeController.js
+
+(function () {
+    "use strict";
+
+    angular.module("app-home")
+        .controller("homeController", homeController);
+
+    function homeController($http) {
+        var vm = this;
+        vm.loading = true;
+        vm.curriculumSheets = [];
+
+        $http.get("/api/curriculum-sheet")
+            .then(function (response) {
+                // success
+                vm.curriculumSheets = response.data;
+            }, function (error) {
+                // failure
+                vm.errorMessage = "Unable to retrieve catalog: " + error;
+            })
+            .finally(function () {
+                vm.loading = false;
+            });
+    }
+
+})();
