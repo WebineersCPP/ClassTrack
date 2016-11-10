7e37ec6d462edfcb94a1d6f08f1bb4bb9769ebda
andreaschmidt95 <andrea_schmidt@ymail.com>
2016-11-10 10:28:24 -0800

    Created FetchCataolog

--- src/ClassTrack/Services/FetchCatalogURLService.cs
+++ src/ClassTrack/Services/FetchCatalogURLService.cs
@@ -5,10 +5,36 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using ClassTrack.Models;
 using System.Net;
+using HtmlAgilityPack;
 
 namespace ClassTrack.Services
 {
     public class FetchCatalogURLService
     {
+        public async Task<string> ReturnUrl(string major, string year, string subplan = null)
+        {
+            HtmlAgilityPack.HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlDocument html;
+
+            try
+            {
+                html = await new HtmlWeb().LoadFromWebAsync(http://catalog.cpp.edu/);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error loading url");
+            }
+
+            var root = html.DocumentNode;
+            var htmlNodes = root.Descendants();
+
+            foreach (HtmlNode node in htmlNodes)
+            {
+                if(node.Name == "select_catalog")
+                {
+
+                }
+            }
+        }
     }
 }
