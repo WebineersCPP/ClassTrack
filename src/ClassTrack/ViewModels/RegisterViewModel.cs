21f68f03bdcfe9593f5ed0a0e393dc252b750117
nliupeng <nliupeng@gmail.com>
2016-11-14 21:54:28 -0800

    register new account feature

---
+++ src/ClassTrack/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassTrack.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        // TODO: Specify validation attributes
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+		[EmailAddress]
+        public string Email { get; set; }
+    }
+}
